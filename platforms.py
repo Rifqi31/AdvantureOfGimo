6d9a5edaa0d9045d83e1a23630d18bf4914e428d
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-17 10:51:28 +0800

    create function for location x, y, width and height for tileset

---
+++ platforms.py
@@ -0,0 +1,35 @@
+# name file : platforms.py
+# python version 3
+
+"""
+Module for managing platforms.
+"""
+
+# import pygame module
+import pygame
+# import spritesheet_functions file
+from spritesheet_functions import SpriteSheet 
+
+# These constants define our platform types:
+#   Name of file
+#   X location of sprite
+#   Y location of sprite
+#   Width of sprite
+#   Height of sprite
+
+# for grass tileset
+GRASS_RIGHT 		= (0 , 0, 70, 70)
+GRASS_LEFT 			= (73, 0, 70, 70)
+GRASS_MIDDLE		= (146, 0, 70, 70)
+GRASS_TOP_DOWN		= (219, 0, 70, 70)
+GRASS_LEFT_RIGHT	= (292, 0, 70, 70)
+GRASS_ROUNDED		= (365, 0, 70, 70)
+
+# for mountain tileset
+SNOW_RIGHT 			= ()
+SNOW_LEFT 			= ()
+SNOW_MIDDLE 		= ()
+SNOW_TOP_DOWN 		= ()
+SNOW_LEFT_RIGHT 	= ()
+SNOW_ROUNDED 		= ()
+
