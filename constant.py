02c08c090d80998a9818938b8887719606477f52
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-17 10:49:17 +0800

    add file constant for indentified size of screen and base color

---
+++ constant.py
@@ -0,0 +1,12 @@
+# name file : constant.py
+# python version 3
+
+# Global constant
+
+# define color
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# size of screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
