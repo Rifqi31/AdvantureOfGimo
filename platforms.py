6c490eb5681412dd5978c780fc5797f9a1d7fda9
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-17 21:11:08 +0800

    added Platform class

--- platforms.py
+++ platforms.py
@@ -93,3 +93,24 @@ DARKLAND_BRICKS 						= (146, 584, 70, 70)
 ANCIENT 								= (365, 584, 70, 70)
 BLOCK 									= (73, 657, 70, 70)
 
+
+
+
+class Platform(pygame.sprite.Sprite):
+	""" Platform the user can jump on """
+
+	def __init__(self, sprite_sheet_data):
+		""" Platform constructor. Assumes constructed with user passing in
+            an array of 5 numbers like what's defined at the top of this
+            code. """
+
+        super().__init__()
+
+        sprite_sheet = SpriteSheet("tiles_asset.png")
+        # Grab the image for this platform
+        self.image = sprite_sheet.get_image(sprite_sheet_data[1],
+        									sprite_sheet_data[2],
+        									sprite_sheet_data[3],
+        									sprite_sheet_data[4])
+
+        self.rect = self.image.get_rect()
