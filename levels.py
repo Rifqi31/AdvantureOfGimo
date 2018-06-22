4ada8f072723048f74f9c40f5e94acc03643d879
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-22 12:51:41 +0800

    delete old file and make one file level

--- levels.py
+++ levels.py
@@ -20,7 +20,7 @@ class Level():
 
 		# How far this world has been scrolled left/right
 		self.world_shift = 0
-		self.level_limit = -1000
+		self.level_limit = 0
 		self.platform_list = pygame.sprite.Group()
 		# self.enemy_list = pygame.sprite.Group()
 		self.player = player
@@ -33,7 +33,7 @@ class Level():
 	def draw(self, screen):
 		""" Draw everything on this level. """
 
-		screen.fill(constants.BLUE)
+		# screen.fill(constants.BLUE)
 		screen.blit(self.background,(self.world_shift // 3,0))
 
 		# Draw all the sprite lists that we have
@@ -48,3 +48,41 @@ class Level():
 		# Go through all the sprite lists and shift
 		for platform in self.platform_list:
 			platform.rect.x += shift_x
+
+
+
+# Create platforms for the level
+class Level_01(Level):
+	""" Definition for level 1. """
+
+	def __init__(self, player):
+		""" Create Level 1 """
+
+		# Call the parent constructor
+		Level.__init__(self, player)
+
+		self.background = pygame.image.load("spritesheet/day_background.png").convert_alpha()
+		self.background.set_colorkey(constants.WHITE)
+		self.level_limit = -1500
+
+			
+		# Array with type of platform, and x, y location of the platform.
+		# for level 01
+		level01 = [[platforms.dirt_wall, -140, 0],
+				[platforms.dirt_land_bottom, 0, 460],
+				[platforms.dirt_small, 670, 460],
+				[platforms.dirt_half_grass, 1110, 460],
+				[platforms.dirt_small_half_grass, 1250, 390],
+				[platforms.one_dirt, 1320, 320],
+				[platforms.dirt_rounded, 1460, 320],
+				[platforms.dirt_rounded, 1600, 320],
+				[platforms.dirt_land_bottom, 1980, 460],
+				[platforms.dirt_big_wall, 2480, 0]]
+		
+
+		for platform in level01:
+			block = platforms.Platform(platform[0])
+			block.rect.x = platform[1]
+			block.rect.y = platform[2]
+			block.player = self.player
+			self.platform_list.add(block)
