f5996203794d661d015b6538622e5f6c436fe534
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-23 15:26:04 +0800

    remove variable screen from main program, and create new one for resolve the error

---
+++ configscreen.py
@@ -0,0 +1,8 @@
+import pygame
+import constants
+
+pygame.init()
+
+# Set the height and width of the screen
+size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
+screen = pygame.display.set_mode(size)
