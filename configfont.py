8640372cda0e72aac00d47d82951a5b5d4bb0a9d
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-23 15:24:06 +0800

    remove font size variable from main program and create one

---
+++ configfont.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+
+# config font
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 30)
+largefont = pygame.font.SysFont("comicsansms", 50)
