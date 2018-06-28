1282d01d44765a96a92858c28a3ab94be494b899
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-06-28 13:28:13 +0800

    added some color codes, and list variable

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 # name file : constant.py
 # python version 3
 
+import pygameMenu 
+from pygameMenu.locals import *
+
 # Global constant
 
 # define color
@@ -9,6 +12,16 @@ WHITE = (255, 255, 255)
 BLUE = (144, 195, 212)
 RED = (200, 0, 0)
 
+# define color for main menu
+DARK_BROWN_DIRT = (46, 23, 6)
+LIGHT_BROWN_DIRT = (135, 69, 21)
+LIGHT_GRASS_GREEN = (35, 188, 39)
+DARK_GRASS_GREEN = (2, 99, 5)
+
 # size of screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+# for about
+ABOUT = ['Author: Rifqi Muttaqin',
+         'Email: muttaqinrifqi31@gmail.com']
