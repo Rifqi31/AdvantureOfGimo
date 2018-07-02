089b649400df2a8707b844f5471a9ad275956206
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-02 13:47:58 +0800

    added sound configuration file

---
+++ configsounds.py
@@ -0,0 +1,11 @@
+# filename : configsounds.py
+# python version 3
+
+# import pygame module
+import pygame
+
+# intialize
+pygame.init()
+
+# load from file
+main_menu_background_music = pygame.mixer.music.load("sounds/01_Invitation.ogg")
