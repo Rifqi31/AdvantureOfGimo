e476dfc3a712d4ee8493ce9b395af1c3dd00c51d
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-03 22:36:07 +0800

    loda sounds file to program

--- configsounds.py
+++ configsounds.py
@@ -9,5 +9,7 @@ pygame.init()
 
 # for music
 background_music = pygame.mixer.Sound("sounds/01_Invitation.ogg")
+
 # for SFX
+menu_sfx = pygame.mixer.Sound("sounds/menu_sfx.wav")
 jump_sfx = pygame.mixer.Sound("sounds/jump_sfx.wav")
