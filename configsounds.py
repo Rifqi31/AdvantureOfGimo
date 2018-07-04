3c713009295d6b4e263bfe728b68a17b81ac1f78
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-04 16:13:49 +0800

    add and loaded new sounds

--- configsounds.py
+++ configsounds.py
@@ -12,4 +12,6 @@ background_music = pygame.mixer.Sound("sounds/01_Invitation.ogg")
 
 # for SFX
 menu_sfx = pygame.mixer.Sound("sounds/menu_sfx.wav")
+push_button_sfx = pygame.mixer.Sound("sounds/push_button_sfx.wav")
 jump_sfx = pygame.mixer.Sound("sounds/jump_sfx.wav")
+game_over_sfx = pygame.mixer.Sound("sounds/game_over_sfx.wav")
