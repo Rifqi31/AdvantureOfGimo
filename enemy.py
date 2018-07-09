46cfb91f87e1a62575803624c681a25054018f4c
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-09 16:41:57 +0800

    added enemy file

---
+++ enemy.py
@@ -0,0 +1,13 @@
+"""
+This module is used to hold the Enemy class. The Enemy represents sprite on the screen.
+"""
+
+# Import pygame and libraries
+from pygame.locals import *
+
+# import pygame module
+import os
+import pygame
+
+from platforms import MovingPlatform
+from spritesheet_functions import SpriteSheet
