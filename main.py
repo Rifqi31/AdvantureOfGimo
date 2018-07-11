f8aa425b6efe17cc798cd6521795fcd787667eb1
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-11 14:29:16 +0800

    add option function from old main program

--- main.py
+++ main.py
@@ -17,6 +17,68 @@ import constants
 
 import platform_scroller
 
+# Option Menu
+def option_menu():
+	""" Function for Option Menu """
+	clock = pygame.time.Clock()
+	
+	# Display Menu
+	display_menu = pygameMenu.Menu(configscreen.screen,
+									bgfun=platform_scroller.main_background,
+									color_selected=constants.WHITE,
+									font=pygameMenu.fonts.FONT_8BIT,
+									font_size=25,
+									font_size_title=30,
+									menu_alpha=100,
+									menu_color=constants.DARK_BROWN_DIRT,
+									menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+									menu_width=int(constants.SCREEN_WIDTH * 0.6),
+									onclose=PYGAME_MENU_DISABLE_CLOSE,
+									option_shadow=False,
+									title='Display Settings',
+									window_height=constants.SCREEN_HEIGHT,
+									window_width=constants.SCREEN_WIDTH)
+	display_menu.add_option('Windowed', platform_scroller.windowed_settings)
+	display_menu.add_option('Fullscreen', platform_scroller.fullscreen_settings)
+	display_menu.add_option('Return to Option', PYGAME_MENU_BACK)
+
+	# Option Menu
+	option_menu = pygameMenu.Menu(configscreen.screen,
+								bgfun=platform_scroller.main_background,
+								color_selected=constants.WHITE,
+								font=pygameMenu.fonts.FONT_8BIT,
+								font_size=25,
+								font_size_title=30,
+								menu_alpha=100,
+								menu_color=constants.DARK_BROWN_DIRT,
+								menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+								menu_width=int(constants.SCREEN_WIDTH * 0.6),
+								onclose=PYGAME_MENU_DISABLE_CLOSE,
+								option_shadow=False,
+								title="Option",
+								window_height=constants.SCREEN_HEIGHT,
+								window_width=constants.SCREEN_WIDTH)
+	option_menu.add_option('Display', display_menu)
+	option_menu.add_option('Back', main_menu)
+
+	while True:
+
+		# Tick
+		clock.tick(60)
+
+		# Application events
+		events = pygame.event.get()
+		for event in events:
+			if event.type == QUIT:
+				exit()
+
+		# Main menu
+		option_menu.mainloop(events)
+
+		# Flip surface
+		pygame.display.flip()
+
+
 
 # Main Menu
 def main_menu():
@@ -95,7 +157,7 @@ def main_menu():
 								window_width=constants.SCREEN_WIDTH)
 
 	main_menu.add_option('Play', play_menu)
-	main_menu.add_option('Option' , platform_scroller.option_menu)
+	main_menu.add_option('Option' , option_menu)
 	main_menu.add_option(about_menu.get_title(), about_menu)
 	main_menu.add_option('Exit', PYGAME_MENU_EXIT)
 
