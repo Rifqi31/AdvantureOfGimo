5ee0c268834bb2ff3f45ae7ac1606c9c1eade8c9
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-18 14:36:51 +0800

    add new font for hud interface

--- configfont.py
+++ configfont.py
@@ -2,7 +2,12 @@ import pygame
 
 pygame.init()
 
-# config font
+# config font for intro game
 smallfont = pygame.font.SysFont("comicsansms", 25)
 medfont = pygame.font.SysFont("comicsansms", 30)
 largefont = pygame.font.SysFont("comicsansms", 50)
+
+# config font for hud screen
+hud_smallfont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 25)
+hud_medfont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 30)
+hud_largefont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 50)
