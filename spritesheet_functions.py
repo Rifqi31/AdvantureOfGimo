3fa8879fe7e53f6c7644435ffd1ad67d56e908d4
Rifqi31 <muttaqinrifqi31@gmail.com>
2018-07-20 07:06:51 +0800

    change function convert() into convert_alpha()

--- spritesheet_functions.py
+++ spritesheet_functions.py
@@ -14,7 +14,8 @@ class SpriteSheet(object):
     def __init__(self, file_name):
         """ Constructor. Pass in the file name of the sprite sheet. """
         # Load the sprite sheet.
-        self.sprite_sheet = pygame.image.load(file_name).convert()
+        # self.sprite_sheet = pygame.image.load(file_name).convert()
+        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
 
 
     def get_image(self, x, y, width, height):
