c6665b6911c7953b095b11d33be29979b18d68e6
rifqi31 <muttaqinrifqi31@gmail.om>
2018-07-25 15:11:42 +0700

    add snow season in level 4

--- platform_scroller.py
+++ platform_scroller.py
@@ -400,6 +400,24 @@ def gameplay():
 			# for player scores
 			settings.hud_msg_to_screen("Scores : " + str(player.scores), constants.WHITE, 600, 0, size="small")
 
+			# process each snow flake in the list
+			for i in range(len(snow_list)):
+
+				# draw the snow flake
+				pygame.draw.circle(configscreen.screen, constants.WHITE, snow_list[i], 2)
+
+				# move the snow flake down one pixel
+				snow_list[i][1] += 1
+
+				# if the snow flake has moved off the bottom of the screen
+				if snow_list[i][1] > 450:
+					# reset it just above the top
+					y = random.randrange(-50, -10)
+					snow_list[i][1] = y
+					# give it new x position
+					x = random.randrange(0, 790)
+					snow_list[i][0] = x
+
 			
 		# Limit to 60 frames per second
 		clock.tick(60)
