cc64eb16a7c101c658e931ce5c8a598a0f50f1bc
rifqi31 <muttaqinrifqi31@gmail.om>
2018-07-28 09:04:54 +0700

    remove SysFont/using font system

--- configfont.py
+++ configfont.py
@@ -3,9 +3,9 @@ import pygame
 pygame.init()
 
 # config font for intro game
-smallfont = pygame.font.SysFont("comicsansms", 25)
-medfont = pygame.font.SysFont("comicsansms", 30)
-largefont = pygame.font.SysFont("comicsansms", 50)
+smallfont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 25)
+medfont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 30)
+largefont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 50)
 
 # config font for hud screen
 hud_smallfont = pygame.font.Font("pygameMenu/fonts/munro.ttf", 25)
