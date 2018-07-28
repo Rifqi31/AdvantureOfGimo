651424a107c16ec2c0cc89158bf11f18ac237566
rifqi31 <muttaqinrifqi31@gmail.om>
2018-07-28 16:32:16 +0700

    add new functions from main game loops

--- configscreen.py
+++ configscreen.py
@@ -6,3 +6,13 @@ pygame.init()
 # Set the height and width of the screen
 size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size)
+
+
+# ----- For Display Settings -----
+def fullscreen_settings():
+	""" This function for fullscreen settings """
+	pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), pygame.FULLSCREEN)
+
+def windowed_settings():
+	""" This function for windowed screen settings """
+	pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
