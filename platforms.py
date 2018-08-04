63c2b036ecbdc5290c3c4bc6b314954aa2e2fafb
rifqi31 <muttaqinrifqi31@gmail.om>
2018-08-04 20:30:42 +0700

    load and add new tilesets, monters and etc

--- platforms.py
+++ platforms.py
@@ -274,7 +274,10 @@ katakana_wa = (657, 438, 70, 70)
 katakana_wo = (657, 511, 70, 70)
 
 # vocal N
-katakana_Na = (730, 438, 70, 70)
+katakana_n = (730, 438, 70, 70)
+
+# restore health
+restore_health = (730, 732, 45, 39)
 
 
 # for enemys
@@ -578,8 +581,80 @@ class Platform_lava_rock(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 
 
+# for moving platform
+class MovingPlatform_dirt(Platform_dirt):
+	""" This is a fancier platform that can actually move. """
+ 
+	def __init__(self, sprite_sheet_data):
+ 
+		super().__init__(sprite_sheet_data)
+ 
+		self.change_x = 0
+		self.change_y = 0
+ 
+		self.boundary_top = 0
+		self.boundary_bottom = 0
+		self.boundary_left = 0
+		self.boundary_right = 0
+ 
+		self.level = None
+		self.player = None
+ 
+	def update(self):
+		""" Move the platform.
+			If the player is in the way, it will shove the player
+			out of the way. This does NOT handle what happens if a
+			platform shoves a player into another object. Make sure
+			moving platforms have clearance to push the player around
+			or add code to handle what happens if they don't. """
+ 
+ 
+		# Move left/right
+		self.rect.x += self.change_x
+ 
+		# See if we hit the player
+		hit = pygame.sprite.collide_rect(self, self.player)
+		if hit:
+			# We did hit the player. Shove the player around and
+			# assume he/she won't hit anything else.
+ 
+			# If we are moving right, set our right side
+			# to the left side of the item we hit
+			if self.change_x < 0:
+				self.player.rect.right = self.rect.left
+			else:
+				# Otherwise if we are moving left, do the opposite.
+				self.player.rect.left = self.rect.right
+ 
+		# Move up/down
+		self.rect.y += self.change_y
+ 
+		# Check and see if we the player
+		hit = pygame.sprite.collide_rect(self, self.player)
+		if hit:
+			# We did hit the player. Shove the player around and
+			# assume he/she won't hit anything else.
+ 
+			# Reset our position based on the top/bottom of the object.
+			if self.change_y < 0:
+				self.player.rect.bottom = self.rect.top
+			else:
+				self.player.rect.top = self.rect.bottom
+ 
+		# Check the boundaries and see if we need to reverse
+		# direction.
+		if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+			self.change_y *= -1
+ 
+		cur_pos = self.rect.x - self.level.world_shift
+		if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+			self.change_x *= -1
 
-class MovingPlatform(Platform_dark_brick):
+
+
+
+
+class MovingPlatform_dark_brick(Platform_dark_brick):
 	""" This is a fancier platform that can actually move. """
  
 	def __init__(self, sprite_sheet_data):
@@ -1021,24 +1096,8 @@ class MovingEnemy(Platform_enemy):
 		# Move left/right
 		self.rect.x += self.change_x
  
-		# See if we hit the player
-		hit = pygame.sprite.collide_rect(self, self.player)
-		if hit:
-			# if the player just stand up/ not move
-			# in front of enemy, he/she will die!
-			# need to fix this
-			gameoverscreen.show_game_over()
- 
 		# Move up/down
 		self.rect.y += self.change_y
- 
-		# Check and see if we the player
-		hit = pygame.sprite.collide_rect(self, self.player)
-		if hit:
-			# if the player just stand up/ not move
-			# in front of enemy, he/she will die!
-			gameoverscreen.show_game_over()
- 
  
 		# Check the boundaries and see if we need to reverse
 		# direction.
@@ -1051,7 +1110,7 @@ class MovingEnemy(Platform_enemy):
 
 
 # for special enemy
-class MovingEnemySpecial(Platform_enemy):
+class MovingEnemySpecial(Platform_special_enemy):
 	""" This is a fancier platform that can actually move. """
  
 	def __init__(self, sprite_sheet_data):
@@ -1073,26 +1132,10 @@ class MovingEnemySpecial(Platform_enemy):
  
 		# Move left/right
 		self.rect.x += self.change_x
- 
-		# See if we hit the player
-		hit = pygame.sprite.collide_rect(self, self.player)
-		if hit:
-			# if the player just stand up/ not move
-			# in front of enemy, he/she will die!
-			# need to fix this
-			gameoverscreen.show_game_over()
- 
+
 		# Move up/down
 		self.rect.y += self.change_y
- 
-		# Check and see if we the player
-		hit = pygame.sprite.collide_rect(self, self.player)
-		if hit:
-			# if the player just stand up/ not move
-			# in front of enemy, he/she will die!
-			gameoverscreen.show_game_over()
- 
- 
+  
 		# Check the boundaries and see if we need to reverse
 		# direction.
 		if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
