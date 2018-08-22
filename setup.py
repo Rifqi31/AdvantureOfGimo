22dc2cea477e61af8b8610c64b91fba8af934661
rifqi31 <muttaqinrifqi31@gmail.om>
2018-08-23 02:29:02 +0700

    create build version

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+import cx_Freeze
+from cx_Freeze import setup, Executable
+
+executables = [cx_Freeze.Executable("main.py")]
+
+cx_Freeze.setup(
+	name="Advanture Of Gimo",
+	version="1.0 Beta",
+	description="Education Game for lean Hiragana & Katakana",
+	author="Rifqi Muttaqin",
+	author_email="muttaqinrifqi31@gmail.com",
+	url="https://github.com/Rifqi31/AdvantureOfGimo",
+	options={"build_exe":{"packages":["pygame"], "include_files":["spritesheet/"]}},
+	executables=executables
+	)
