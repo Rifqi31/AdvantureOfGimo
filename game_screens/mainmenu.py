b2a2be5675d9a91f4b07fc08476ca61f4d6ed87a
rifqi31 <muttaqinrifqi31@gmail.om>
2018-09-03 20:19:53 +0700

    add new menu, select levels

--- game_screens/mainmenu.py
+++ game_screens/mainmenu.py
@@ -16,6 +16,36 @@ from game_settings import configscreen
 from hiragana_mode import platform_scroller_hiragana
 from katakana_mode import platform_scroller_katakana
 
+# import level hiragana
+from hiragana_mode.levelplay.mainlevel import (
+    play_hiragana_level_1,
+    play_hiragana_level_2,
+    play_hiragana_level_3,
+    play_hiragana_level_4,
+    play_hiragana_level_5,
+    play_hiragana_level_6,
+    play_hiragana_level_7,
+    play_hiragana_level_8,
+    play_hiragana_level_9,
+    play_hiragana_level_10,
+    play_hiragana_level_11
+)
+
+# import level katakana
+from katakana_mode.levelplay.mainlevel import (
+    play_katakana_level_1,
+    play_katakana_level_2,
+    play_katakana_level_3,
+    play_katakana_level_4,
+    play_katakana_level_5,
+    play_katakana_level_6,
+    play_katakana_level_7,
+    play_katakana_level_8,
+    play_katakana_level_9,
+    play_katakana_level_10,
+    play_katakana_level_11
+)
+
 
 def main_background():
     """
@@ -96,6 +126,328 @@ def option_menu():
         pygame.display.flip()
 
 
+# select level hiragana
+def select_level_hiragana_display1():
+    """ Function for Select level in hiragana mode"""
+    clock = pygame.time.Clock()
+
+    # Display Menu
+    select_level_hiragana_page1 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+    # LEVEL PAGE 1
+    select_level_hiragana_page1.add_option(
+        'Play All Leves', platform_scroller_hiragana.gameplay)
+    select_level_hiragana_page1.add_option(
+        'Level 01 ', play_hiragana_level_1.gameplay)
+    select_level_hiragana_page1.add_option(
+        'Level 02', play_hiragana_level_2.gameplay)
+    select_level_hiragana_page1.add_option(
+        'Level 03', play_hiragana_level_3.gameplay)
+    select_level_hiragana_page1.add_option(
+        'Level 04', play_hiragana_level_4.gameplay)
+    select_level_hiragana_page1.add_option(
+        'Next', select_level_hiragana_display2)
+    select_level_hiragana_page1.add_option(
+        'Back', main_menu)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_hiragana_page1.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
+def select_level_hiragana_display2():
+    """ Function for Select level in hiragana mode"""
+    clock = pygame.time.Clock()
+
+    select_level_hiragana_page2 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+
+    # LEVEL PAGE 2
+    select_level_hiragana_page2.add_option(
+        'Level 05', play_hiragana_level_5.gameplay)
+    select_level_hiragana_page2.add_option(
+        'Level 06', play_hiragana_level_6.gameplay)
+    select_level_hiragana_page2.add_option(
+        'Level 07', play_hiragana_level_7.gameplay)
+    select_level_hiragana_page2.add_option(
+        'Level 08', play_hiragana_level_8.gameplay)
+    select_level_hiragana_page2.add_option(
+        'Level 09', play_hiragana_level_9.gameplay)
+    select_level_hiragana_page2.add_option(
+        'Next', select_level_hiragana_display3)
+    select_level_hiragana_page2.add_option(
+        'Back', select_level_hiragana_display1)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_hiragana_page2.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
+def select_level_hiragana_display3():
+    """ Function for Select level in hiragana mode"""
+    clock = pygame.time.Clock()
+
+    select_level_hiragana_page3 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+
+    # LEVEL PAGE 3
+    select_level_hiragana_page3.add_option(
+        'Level 10', play_hiragana_level_10.gameplay)
+    select_level_hiragana_page3.add_option(
+        'Level 11', play_hiragana_level_11.gameplay)
+    select_level_hiragana_page3.add_option(
+        'Back', select_level_hiragana_display2)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_hiragana_page3.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
+# select level katakana
+def select_level_katakana_display1():
+    """ Function for Select level in katakana mode"""
+    clock = pygame.time.Clock()
+
+    # Display Menu
+    select_level_katakana_page1 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+    # LEVEL PAGE 1
+    select_level_katakana_page1.add_option(
+        'Play All Leves', platform_scroller_katakana.gameplay)
+    select_level_katakana_page1.add_option(
+        'Level 01 ', play_katakana_level_1.gameplay)
+    select_level_katakana_page1.add_option(
+        'Level 02', play_katakana_level_2.gameplay)
+    select_level_katakana_page1.add_option(
+        'Level 03', play_katakana_level_3.gameplay)
+    select_level_katakana_page1.add_option(
+        'Level 04', play_katakana_level_4.gameplay)
+    select_level_katakana_page1.add_option(
+        'Next', select_level_katakana_display2)
+    select_level_katakana_page1.add_option(
+        'Back', main_menu)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_katakana_page1.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
+def select_level_katakana_display2():
+    """ Function for Select level in katakana mode"""
+    clock = pygame.time.Clock()
+
+    select_level_katakana_page2 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+
+    # LEVEL PAGE 2
+    select_level_katakana_page2.add_option(
+        'Level 05', play_katakana_level_5.gameplay)
+    select_level_katakana_page2.add_option(
+        'Level 06', play_katakana_level_6.gameplay)
+    select_level_katakana_page2.add_option(
+        'Level 07', play_katakana_level_7.gameplay)
+    select_level_katakana_page2.add_option(
+        'Level 08', play_hiragana_level_8.gameplay)
+    select_level_katakana_page2.add_option(
+        'Level 09', play_katakana_level_9.gameplay)
+    select_level_katakana_page2.add_option(
+        'Next', select_level_katakana_display3)
+    select_level_katakana_page2.add_option(
+        'Back', select_level_katakana_display1)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_katakana_page2.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
+def select_level_katakana_display3():
+    """ Function for Select level in katakana mode"""
+    clock = pygame.time.Clock()
+
+    select_level_katakana_page3 = pygameMenu.Menu(
+        configscreen.screen,
+        bgfun=main_background,
+        color_selected=constants.WHITE,
+        font=pygameMenu.fonts.FONT_8BIT,
+        font_size=25,
+        font_size_title=30,
+        menu_alpha=100,
+        menu_color=constants.DARK_BROWN_DIRT,
+        menu_height=int(constants.SCREEN_HEIGHT * 0.6),
+        menu_width=int(constants.SCREEN_WIDTH * 0.6),
+        onclose=PYGAME_MENU_DISABLE_CLOSE,
+        option_shadow=False,
+        title='Select Levels',
+        window_height=constants.SCREEN_HEIGHT,
+        window_width=constants.SCREEN_WIDTH
+    )
+
+    # LEVEL PAGE 3
+    select_level_katakana_page3.add_option(
+        'Level 10', play_katakana_level_10.gameplay)
+    select_level_katakana_page3.add_option(
+        'Level 11', play_katakana_level_11.gameplay)
+    select_level_katakana_page3.add_option(
+        'Back', select_level_katakana_display2)
+
+    while True:
+
+        # Tick
+        clock.tick(60)
+
+        # Application events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                exit()
+
+        # Main menu
+        select_level_katakana_page3.mainloop(events)
+
+        # Flip surface
+        pygame.display.flip()
+
+
 # Main Menu
 def main_menu():
     """ Function for Main Menu """
@@ -129,8 +481,9 @@ def main_menu():
         window_height=constants.SCREEN_HEIGHT,
         window_width=constants.SCREEN_WIDTH
     )
-    play_menu.add_option('Hiragana', platform_scroller_hiragana.gameplay)
-    play_menu.add_option('Katakana', platform_scroller_katakana.gameplay)
+    # play_menu.add_option('Hiragana', platform_scroller_hiragana.gameplay)
+    play_menu.add_option('Hiragana', select_level_hiragana_display1)
+    play_menu.add_option('Katakana', select_level_katakana_display1)
     play_menu.add_option('Back', PYGAME_MENU_BACK)
 
     # About Menu
